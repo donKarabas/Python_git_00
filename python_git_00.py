4f8b3091348dd90c43e0f4953c4f5fc8021acf27
Карабас <da_om@mail.ru>
2023-08-20 13:53:00 +0300

    Коммент 3

--- python_git_00.py
+++ python_git_00.py
@@ -1,331 +1,273 @@
 
-# https://habr.com/ru/articles/137415/; Пользовательские атрибуты в Python.
+# Построим несколько домов, добавим в них комнат, а затем выполним сравнения.
+
+from functools import total_ordering
+
+class Room:
+    def __init__(self, name, length, width):
+        self.name = name
+        self.length = length
+        self.width = width
+        self.square_feet = self.length * self.width
+
+@total_ordering
+class House:
+    def __init__(self, name, style):
+        self.name = name
+        self.style = style
+        self.rooms = list()
+
+    @property
+    def living_space_footage(self):
+        return sum(r.square_feet for r in self.rooms)
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def __str__(self):
+        return '{}: {} square foot {}'.format(self.name,
+                                              self.living_space_footage,
+                                              self.style)
+
+    def __eq__(self, other):
+        return self.living_space_footage == other.living_space_footage
+
+    def __lt__(self, other):
+        return self.living_space_footage < other.living_space_footage
 
 
-# __dict__
+# Построим несколько домов и добавим в них комнаты
+h1 = House('h1', 'Cape')
+h1.add_room(Room('Master Bedroom', 14, 21))
+h1.add_room(Room('Living Room', 18, 20))
+h1.add_room(Room('Kitchen', 12, 16))
+h1.add_room(Room('Office', 12, 12))
+h2 = House('h2', 'Ranch')
+h2.add_room(Room('Master Bedroom', 14, 21))
+h2.add_room(Room('Living Room', 18, 20))
+h2.add_room(Room('Kitchen', 12, 16))
+h3 = House('h3', 'Split')
+h3.add_room(Room('Master Bedroom', 14, 21))
+h3.add_room(Room('Living Room', 18, 20))
+h3.add_room(Room('Office', 12, 16))
+h3.add_room(Room('Kitchen', 15, 17))
 
-class StuffHolder:
-    stuff = 'class stuff'
+houses = [h1, h2, h3]
 
-a = StuffHolder()
-b = StuffHolder()
-
-print(a.stuff)      # class stuff
-print(b.stuff)      # class stuff
-
-b.b_stuff = 'b stuff'
-
-print(b.b_stuff)    # b stuff
-print(a.b_stuff)
-# Traceback (most recent call last):
-#   File "/home/lazutchik/PycharmProjects/data_files/python_git_00.py", line 19, in <module>
-#     print(a.b_stuff)
-# AttributeError: 'StuffHolder' object has no attribute 'b_stuff'
-
-print(StuffHolder.__dict__)
-# {... 'stuff': 'class stuff', ...}
-# {'__module__': '__main__', 'stuff': 'class stuff', '__dict__': <attribute '__dict__' of 'StuffHolder' objects>,
-# '__weakref__': <attribute '__weakref__' of 'StuffHolder' objects>, '__doc__': None}
-
-print(a.__dict__)       # {}
-print(b.__dict__)       # {'b_stuff': 'b stuff'}
-
-print(a.__class__)      # <class '__main__.StuffHolder'>
-print(b.__class__)      # <class '__main__.StuffHolder'>
-
-print(a.new_stuff)
-# Traceback (most recent call last):
-#   File "/home/lazutchik/PycharmProjects/data_files/test04.py", line 10, in <module>
-#     print(a.new_stuff)
-# AttributeError: 'StuffHolder' object has no attribute 'new_stuff'
-
-print(b.new_stuff)
-# Traceback (most recent call last):
-#   File "/home/lazutchik/PycharmProjects/data_files/test04.py", line 10, in <module>
-#     print(b.new_stuff)
-# AttributeError: 'StuffHolder' object has no attribute 'new_stuff'
-
-StuffHolder.new_stuff = 'new'
-
-print(StuffHolder.__dict__)
-# {... 'stuff': 'class stuff', 'new_stuff': 'new', ...}
-# {'__module__': '__main__', 'stuff': 'class stuff', '__dict__': <attribute '__dict__' of 'StuffHolder' objects>,
-# '__weakref__': <attribute '__weakref__' of 'StuffHolder' objects>, '__doc__': None, 'new_stuff': 'new'}
-
-print(a.new_stuff)  # new
-print(b.new_stuff)  # new
-
-c = StuffHolder()
-print(c.__dict__)   # {}
-
-c.stuff = 'more c stuff'
-print(c.__dict__)   # {'stuff': 'more c stuff'}
-print(StuffHolder.__dict__)     # {... 'stuff': 'more stuff', 'new_stuff': 'new', ...}
-
-d = StuffHolder()
-print(d.stuff)  # class stuff
-
-d.stuff = 'd stuff'
-print(d.stuff)      # d stuff
-
-#-------------------------------------------------------------------
-# Дескрипторы.
-
-class FuncHolder:
-    def func(self):
-        pass
-
-fh = FuncHolder()
-print(FuncHolder.func)  # <function FuncHolder.func at 0x7f26df240ca0>
-print(FuncHolder.__dict__)
-# {... 'func': <function FuncHolder.func at 0x7fe8568fdca0>, ...}
-# {'__module__': '__main__', 'func': <function FuncHolder.func at 0x7fe8568fdca0>,
-# '__dict__': <attribute '__dict__' of 'FuncHolder' objects>,
-# '__weakref__': <attribute '__weakref__' of 'FuncHolder' objects>, '__doc__': None}
-
-print(fh.func)  # <bound method FuncHolder.func of <__main__.FuncHolder object at 0x7f62c7f66dc0>>
-
-print(FuncHolder.func.__class__.__get__)    # <slot wrapper '__get__' of 'function' objects>
-
-# descr.__get__(self, obj, type=None) --> value
-# - переопределяет способ получения значения атрибута)
-# descr.__set__(self, obj, value) --> None
-# - переопределяет способ присваивания значения атрибуту)
-# descr.__delete__(self, obj) --> None
-# - переопределяет способ удаления атрибута)
-
-#--------------------------------------------------------------
-
-# Дескрипторы данных - Data Descriptor
-# - объект, который реализует метод __get__() и __set__()
-
-class DataDesc:
-    def __get__(self, obj, cls):
-        print('Trying to access from {0} class {1}'.format(obj, cls))
-
-    def __set__(self, obj, val):
-        print('Trying to set {0} for {1}'.format(val, obj))
-
-    def __delete__(self, obj):
-        print('Trying to delete from {0}'.format(obj))
-
-class DataHolder:
-    data = DataDesc()
-
-d = DataHolder()
-
-print(DataHolder.data)
-# Trying to access from None class <class '__main__.DataHolder'>
-# None
-
-print(d.data)
-# Trying to access from <__main__.DataHolder object at 0x7f4fa970ffd0> class <class '__main__.DataHolder'>
-# None
-d.data
-# Trying to access from <__main__.DataHolder object at 0x7fb8bf2d8fd0> class <class '__main__.DataHolder'>
-
-d.data = 1  # Trying to set 1 for <__main__.DataHolder object at 0x7f457e317fd0>
-del d.data  # Trying to delete from <__main__.DataHolder object at 0x7f5d7e6c8fd0>
-
-d.__dict__['data'] = 'override!'
-
-print(d.__dict__)   # {'data': 'override!'}
-d.data
-# Trying to access from <__main__.DataHolder object at 0x7fb4434a4fd0> class <class '__main__.DataHolder'>
-
-print(DataHolder.__dict__)  # {... 'data': <__main__.DataDesc object at 0x7f3a275efdc0>, ...}
-DataHolder.data = 'kick descriptor out'
-print(DataHolder.__dict__)  # {... 'data': 'kick descriptor out', ...}
-print(DataHolder.data)  # kick descriptor out
-
-#-------------------------------------------------------------------------
-
-# Дескрипторы не данных - Non-Data Descriptor
-# - объект, который реализует метод __get__()
-
-class NonDataDesc:
-    def __get__(self, obj, cls):
-        print('Trying to access from {0} class {1}'.format(obj, cls))
-
-class NonDataHolder:
-    non_data = NonDataDesc()
-
-n = NonDataHolder()
-
-NonDataHolder.non_data
-# Trying to access from None class <class '__main__.NonDataHolder'>
-
-n.non_data
-# Trying to access from <__main__.NonDataHolder object at 0x7faf11b0dfd0> class <class '__main__.NonDataHolder'>
-
-n.non_data = 1
-print(n.non_data)   # 1
-print(n.__dict__)   # {'non_data': 1}
+print('Is h1 bigger than h2?', h1 > h2)     # Is h1 bigger than h2? True
+print('Is h2 smaller than h3?', h2 < h3)    # Is h2 smaller than h3? True
+print('Is h2 greater than or equal to h1?', h2 >= h1)   # Is h2 greater than or equal to h1? False
+print('Which one is biggest?', max(houses)) # Which one is biggest? h3: 1101 square foot Split
+print('Which is smallest?', min(houses))    # Which is smallest? h2: 846 square foot Ranch
 
 #---
-# Создать свойство просто с помощью дескриптора.
+# Методы, созданные @total_ordering
+__le__ = lambda self, other: self <  other or self == other
+__gt__ = lambda self, other: not (self < other or self == other)
+__ge__ = lambda self, other: not (self < other)
+__ne__ = lambda self, other: not self == other
 
-class Descriptor:
-    def __get__(self, obj, type):
-        print('getter used')
+#-------------------------------------------------------------------------------
+# Мой код. Монеты coin по Геддису. Оформление for, toss, print
 
-    def __set__(self, obj, val):
-        print('setter used')
+import random
 
-    def __delete__(self, obj):
-        print('deleter used')
+class Coin:
+    def __init__(self):
+        self.__sideup = 'Орёл'
+
+    def toss(self):
+        if random.randint(0, 1) == 0:
+            self.__sideup = 'Орёл'
+        else:
+            self.__sideup = 'Решка'
+
+    def get_sideup(self):
+        return self.__sideup
+
+coin1 = Coin()
+coin2 = Coin()
+coin3 = Coin()
+
+print('Вот три монеты, у которых эти стороны обращены вверх:')
+print(coin1.get_sideup())
+print(coin2.get_sideup())
+print(coin3.get_sideup())
+print()
+
+print('Подбрасываю все три монеты по 5 раз')
+print()
+for x in range(5): coin1.toss(), print(coin1.get_sideup())
+print()
+for x in range(5): coin2.toss(), print(coin2.get_sideup())
+print()
+for x in range(5): coin3.toss(), print(coin3.get_sideup())
+
+# Вот три монеты, у которых эти стороны обращены вверх:
+# Орёл
+# Орёл
+# Орёл
+#
+# Подбрасываю все три монеты по 5 раз
+#
+# Решка
+# Орёл
+# Решка
+# Решка
+# Орёл
+#
+# Орёл
+# Решка
+# Решка
+# Орёл
+# Решка
+#
+# Решка
+# Орёл
+# Орёл
+# Решка
+# Решка
+
+# --------------------------------------------------------------
+
+# https://pythonclass.ru/python/klassy-v-python/#init
+
+class Car:
+    '''Описание автомобиля'''
+    def __init__(self, brand, model, years):
+        '''Инициализирует атрибуты'''
+        self.brand = brand
+        self.model = model
+        self.years = years
+        self.mileage = 0
+
+    def get_full_name(self):
+        '''Автомобиль'''
+        name = f'Автомобиль {self.brand} {self.model} {self.years}'
+        return name.title()
+
+    def read_mileage(self):
+        '''Пробег автомобиля'''
+        print(f'Пробег автомобиля {self.mileage} км.')
+
+    def update_mileage(self, new_mileage):
+        '''Устанавливает новое значение пробега'''
+        self.mileage = new_mileage
+
+    def add_mileage(self, km):
+        '''Добавляет пробег'''
+        self.mileage += km
 
 
-class MyClass:
-    prop = Descriptor()
+car_2 = Car('audi', 'a4', 2019)
+print(car_2.get_full_name())    # Автомобиль Audi A4 2019
+car_2.read_mileage()            # Пробег автомобиля 0 км.
+car_2.update_mileage(17100)
+car_2.read_mileage()            # Пробег автомобиля 17100 км.
+car_2.add_mileage(14687)
+car_2.read_mileage()            # Пробег автомобиля 31787 км.
 
-#---
-# Или воспользоваться встроенным классом property,
-# он представляет собой дескриптор данных.
+print()
 
-class MyClass:
+class ElectricCar(Car):
+    '''Описывает электромобиль'''
+    def __init__(self, brand, model, years):
+        '''Инициализирует атрибуты класса родителя'''
+        super().__init__(brand, model, years)
+        # атрибут класса-потомка
+        self.battery_size = 100
 
-    def _getter(self):
-        print("getter used")
-    def _setter(self, val):
-        print("setter used")
-    def _deleter(self):
-        print("deleter used")
+    def battery_power(self):
+        '''Выводит мощность аккумулятора авто'''
+        print(f'Мощность аккумулятора {self.battery_size} Квт.ч')
 
-    prop = property(_getter, _setter, _deleter, "doc string")
+# tesla_1 = ElectricCar('tesla', 'model x', 2021)
+# print(tesla_1.get_full_name())  # Автомобиль Tesla Model X 2021
+# tesla_1.battery_power()  # Мощность аккумулятора 100 Квт.ч
+
+    def get_full_name(self):
+        '''Переопределяет метод класса-родителя'''
+        # Автомобиль
+        name = (f'Автомобиль {self.brand} {self.model} {self.years} '
+                f'{self.battery_size}-Квт.ч')
+        return name.title()
+
+# При запросе полного названия автомобиля, Python проигнорирует метод
+# def get_full_name() в классе-родителе Car и сразу перейдёт к методу
+# def get_full_name() в классе ElectricCar.
+
+tesla_1 = ElectricCar('tesla', 'model x', 2021)
+print(tesla_1.get_full_name())      # Автомобиль Tesla Model X 2021 100-Квт.Ч
+
+#---------------------------------------------------------
+# Класс принмает текст из файла, определяет уникальные слова, считает
+# количество повторов каждого уникального слова в тексте, сортирует
+# уникальные слова по повторам от бошьшего к меньшему.
+
+class WordCount:
+    def __init__(self, FILEIN, FILEOUT):
+        self.FILEIN = FILEIN
+        self.FILEOUT = FILEOUT
+
+    def set_filein(self, FILEIN):
+        self.FILEIN = FILEIN
+        text_file = open(FILEIN, 'r')
+        text = text_file.read()
+        text_file.close()  # Получаем текст из файла
+        text = text.rstrip('\n')
+        text = text.rstrip('\ufeff')
+
+        self.ls_text = text.split()
+
+        import re
+
+        search = '-'  # Убираем дефис как отдельное слово
+        for i in self.ls_text:
+            if i == search:
+                self.ls_text.remove(i)  # Переводим слова в нижний регистр. Сортируем
+        self.ls_text2 = [i.lower() for i in self.ls_text]  # текст по словам.
+        self.ls_text2 = [re.sub(r'[.,"?:!;()]', '', i) for i in self.ls_text2]
+
+        self.set_text = {i for i in self.ls_text2}  # Уникальные слова элементами множества.
+
+    def set_fileout(self, FILEOUT):
+        import dct_sort
+        self.FILEOUT = FILEOUT
+        str_stat = f'Всего слов: {len(self.ls_text2)} | Уникальных слов: {len(self.set_text)}'
+        str_uniq = '''
+        Вот список слов, из которых состоит текст:
+        -------------'''
+        str_repeat = '''------------
+        Вот количество повторов слов по убыванию:
+        '''
+
+        dct_repeat = {}
+        for let in self.set_text:
+            count = 0
+            for item in self.ls_text:
+                if let == item:  # Создаём словарь повторений
+                    count += 1  # Сортируем по убыванию
+            dct_repeat[let] = count
+        dct_rev = dct_sort.dict_sorted_reverse(dct_repeat)
+
+        word_file = open(FILEOUT, 'w')
+
+        word_file.write(f'{str_stat}\n')
+        word_file.write(f'{str_uniq}\n')
+        word_file.write(f'{self.set_text}\n')  # Записываем результаты в файл.
+        word_file.write(f'{str_repeat}\n')
+
+        for k in dct_rev:
+            word_file.write(f'{k} - {dct_rev[k]}\n')
+
+        word_file.close()
+        print('Файл записан')
 
 
-m = MyClass()
+FILEIN = 'strings.txt'
+FILEOUT = 'strings_count.txt'
 
-m.prop          # getter used
-m.prop = 1      # setter used
-del m.prop      # deleter used
+word_count = WordCount(FILEIN, FILEOUT)
+word_count.set_filein(FILEIN)
+word_count.set_fileout(FILEOUT)
 
-#---
-# Дескриптор данных property без каких-либо функций
-
-class MySecondClass:
-    prop = property()
-
-m2 = MySecondClass()
-
-m2.prop     # AttributeError: unreadable attribute
-m2.prop = 1   # AttributeError: can't set attribute
-del m2      # (AttributeError: can't delete attribute)??
-
-#---
-# Дескрипторы classmethod, staticmethod
-
-class StaticAndClassMethodHolder:
-
-    def _method(*args):
-        print('_method called with ', args)
-
-    static = staticmethod(_method)
-    cls = classmethod(_method)
-
-s = StaticAndClassMethodHolder()
-
-s._method()
-# _method called with (<__main__.StaticAndClassMethodHolder object at 0x7f41d7f91e20>,)
-
-s.static()  # _method called with ()
-s.cls()
-# _method called with (<class '__main__.StaticAndClassMethodHolder'>,)
-
-#---------------------------------------------------------------------------
-
-# Методы __getattr__(), __setattr__(), __delattr__() и __getattribute__()
-
-# __getattr__(self, name) будет вызван в случае, если запрашиваемый атрибут не
-# найден обычным механизмом (в __dict__ экземпляра, класса и т.д.)
-
-class SmartyPants:
-    def __getattr__(self, attr):
-        print('Yep, I know', attr)
-    tellme = 'It`s a secret'
-
-smarty = SmartyPants()
-smarty.name = 'Smartinius Smart'
-
-smarty.quicksort    # Yep, I know quicksort
-smarty.python       # Yep, I know python
-print(smarty.tellme)    # It`s a secret
-print(smarty.name)      # Smartinius Smart
-
-#---
-# Следует иметь в виду, что вызов специальных методов (например __len__(), __str__()) через встроенные
-# функции или неявный вызов через синтаксис языка осуществляется в обход __getattribute__()
-
-class Optimist:
-    attr = 'class attribute'
-
-    def __getattribute__(self, name):
-        print('{0} is great!'.format(name))
-
-    def __len__(self):
-        print('__len__ is special')
-        return 0
-
-o = Optimist()
-o.instance_attr = 'instance'
-
-o.attr          # attr is great!
-o.dark_beer     # dark_beer is great!
-o.instance_attr # instance_attr is great!
-o.__len__       # __len__ is great!
-len(o)          # __len__ is special (__len__ is special\n 0)??
-
-#---
-# __setattr__(self, name, value) будет вызван при попытке установить значение атрибута экземпляра.
-
-class NoSetters:
-    attr = 'class attribute'
-    def __setattr__(self, name, val):
-        print('not setting {0}={1}'.format(name, val))
-
-no_setters = NoSetters()
-
-no_setters.a = 1            # not setting a=1
-no_setters.attr = 1         # not setting attr=1
-print(no_setters.__dict__)  # {}
-print(no_setters.attr)      # class attribute
-no_setters.a    # AttributeError: 'NoSetters' object has no attribute 'a'
-
-#---
-# При переопределении __getattribute__(), __setattr__(), и __delattr__() следует иметь в виду, что
-# стандартный способ получения доступа к атрибутам можно вызвать через object
-
-class GentleGuy:
-    def __getattribute__(self, name):
-        if name.endswith('_please'):
-            return object.__getattribute__(self, name.replace('_please', ''))
-        raise AttributeError('And the magic word!?')
-
-gentle = GentleGuy()
-gentle.coffee = 'some coffee'
-
-gentle.coffee   # AttributeError: And the magic word!?
-print(gentle.coffee_please) # some coffee
-
-# ----------------------------------------------------------------------------------
-
-# __slots__
-# # Наличие __slots__ ограничивает возможные имена атрибутов объекта теми, которые там указаны,
-# # и снимает необходимость создавать __dict__ экземпляра.
-
-class Slotter:
-    __slots__ = ['a', 'b']
-
-s = Slotter()
-
-print(s.__dict__)   # AttributeError: 'Slotter' object has no attribute '__dict__'
-s.c = 1     # AttributeError: 'Slotter' object has no attribute 'c'
-s.a = 1
-s.b = 1
-print(s.a, ';', s.b)    # 1 ; 1
-print(dir(s))   # [..., 'a', 'b']
-# ['__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__',
-# '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__',
-# '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__slots__',
-# '__str__', '__subclasshook__', 'a', 'b']
